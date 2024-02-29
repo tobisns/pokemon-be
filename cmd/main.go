717fdb3c0c79036178f2a3a653ac4846073610ed
tobias ns <13521090@std.stei.itb.ac.id>
2024-02-29 18:04:35 +0700

    fix: use env var in main

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"learngo/pkg/api"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	port, err := strconv.Atoi(os.Getenv("DB_PORT"))
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	api.Start(&api.Config{
-		DBHost:     "pokemon_db",
-		DBPort:     5432,
-		DBUser:     "postgres",
-		DBPassword: "lololol",
-		DBName:     "pokemon_db",
+		DBHost:     os.Getenv("DB_HOST"),
+		DBPort:     port,
+		DBUser:     os.Getenv("POSTGRES_USER"),
+		DBPassword: os.Getenv("POSTGRES_PASSWORD"),
+		DBName:     os.Getenv("POSTGRES_DB"),
 	})
 }
--- go.mod
+++ go.mod
@@ -7,3 +7,5 @@ require (
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/lib/pq v1.10.9
 )
+
+require github.com/joho/godotenv v1.5.1 // indirect
